c6c84e262bbf17213fa2effb12cb6bb34f0e3f7f
Adrian <Rodriguez.Adrian609@gmail.com>
2018-05-14 12:25:27 -0400

    Adding sort and dropdown to sort by language

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const ul = document.getElementById("repos");
 const url = "https://api.github.com/users/adrian609/repos";
-
+const selector = document.getElementById("inlineFormCustomSelect");
+var repoArray = [];
 function createNode(element) {
     return document.createElement(element);
 }
@@ -10,7 +11,7 @@ function append(parent, el) {
 }
 
 fetch(url).then(response => response.json()).then(function (data) {
-    console.log(data);
+    //console.log(data);
     let repos = data; // Get the results
     return repos.map(function (repo) { // Map through the results and for each run the code below
 
@@ -18,19 +19,38 @@ fetch(url).then(response => response.json()).then(function (data) {
             repoURL = repo.html_url,
             repoLang = repo.language;
 
-        let li = createNode('li'),
-            button = createNode('button');
+        createList(repoTitle, repoLang, repoURL);
 
-        button.innerHTML = "View On GitHub";
-        li.innerHTML = `Title: ${repoTitle}  <br> Language:  ${repoLang}`; // Make the HTML of our span to be the first and last name of our author
-        li.classList.add('list-group-item');
-        li.classList.add('text-left');
-        button.classList.add('btn');
-        button.classList.add("btn-outline-primary");
-        button.classList.add("float-right")
-        append(li, button);
-        append(ul, li);
+
     });
 }).catch(function (error) {
     console.log(error);
-});   
+});
+
+function createList(repoTitle, repoLang, repoURL) {
+    //console.log(repoTitle, repoLang, repoURL);
+   
+    new repoList();
+
+    var repoList = {
+        name: repoTitle,
+        language: repoLang,
+        url: repoURL
+    };
+    console.log(repoArray);
+
+    let li = createNode('li'),
+        button = createNode('button');
+
+    button.innerHTML = "View On GitHub";
+    li.innerHTML = `Title: ${repoList.name}  <br> Language:  ${repoList.language}`;
+    li.classList.add('list-group-item');
+    li.classList.add('text-left');
+    button.classList.add('btn');
+    button.classList.add("btn-outline-primary");
+    button.classList.add("float-right")
+    append(li, button);
+    append(ul, li);
+    //append(selector, op);
+}
+console.log(repoArray.length);
