83425ef592130045bd82353f98d92220c5e6dbec
Adrian <Rodriguez.Adrian609@gmail.com>
2018-05-15 12:41:26 -0400

    Added a node serve and Watson modules

--- js/demos.js
+++ js/demos.js
@@ -2,7 +2,7 @@
 
 const div = document.getElementById("fetch-demo");
 const url = "https://api.iextrading.com/1.0/stock/aapl/quote";
-
+const url2 = "https://stats.nba.com/stats/commonallplayers";
 function createNode(element) {
     return document.createElement(element);
 }
@@ -19,31 +19,9 @@ fetch(url).then(response => response.json()).then(function (data) {
     creatList(repos);
 }).catch(function (error) {
     console.log(error);
+    alert('There was a problem getting info! *If you are using IE or Edge this demo will not work')
 });
 
-function postData(url, data) {
-    // Default options are marked with *
-    return fetch(url, {
-        body: JSON.stringify(data), // must match 'Content-Type' header
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, same-origin, *omit
-        headers: {
-            'user-agent': 'Mozilla/4.0 MDN Example',
-            'content-type': 'application/json'
-        },
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, cors, *same-origin
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // *client, no-referrer
-    })
-        .then(response => response.json()) // parses response to JSON
-}
-
-
-
-function fetchDemo() {
-
-}
 function creatList(repos) {
 
     var name = repos.companyName,
